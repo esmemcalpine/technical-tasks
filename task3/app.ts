b5a5c096c2275074d2eb65feb8af234fcd4c047e
Esme <esmemcalpine@hotmail.co.uk>
2024-02-25 20:52:38 +0000

    cleaning up

--- task3/app.ts
+++ task3/app.ts
@@ -11,23 +11,11 @@ app.listen(port, () => {
 const accountRoutes = require("./routes/accountRoutes");
 
 
-/* This is the root route. It is used to check if the server is running. */
 app.get("/", (req, res) => {
   res.status(200).json({ alive: "True" });
 });
 
-/* Telling the server to use the routes in the ProductRoutes file. */
 app.use("/", accountRoutes);
 
 
 export default app;
-
-
-
-
-/* A middleware that parses the body of the request and makes it available in the req.body object. */
-// app.use(express.json());
-
-
-
-// module.exports = app;
